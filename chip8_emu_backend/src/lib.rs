87a5686e62dbf21e1db11caf004b11f6560b6d32
SpeakBoy <nevinspeakman@outlook.com>
2025-05-24 13:13:10 -0400

    Setup Cpu struct

--- chip8_emu_backend/src/lib.rs
+++ chip8_emu_backend/src/lib.rs
@@ -1,14 +1,52 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+// 64x32 display
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+
+// 4 KB of RAM
+const RAM_SIZE: usize = 4096;
+// 16 V Registers
+const NUM_V_REGS: usize = 16;
+const STACK_SIZE: usize = 16;
+// 16 key input
+const NUM_KEYS: usize = 16;
+
+pub struct Cpu {
+    // 16-bit program counter
+    pc: u16,
+    // RAM is in bytes (8 bits) of RAM_SIZE size
+    ram: [u8; RAM_SIZE],
+    // monochrome display only requires 1 bit values (boolean)
+    // for colors (i.e., black and white)
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    // 8-bit registers
+    v_reg: [u8; NUM_V_REGS],
+    // 16-bit indexing register
+    i_reg: u16,
+    // 16-bit stack
+    stack: [u16; STACK_SIZE],
+    // keys are either pressed (true) or not pressed (false)
+    keys: [bool; NUM_KEYS],
+    // 8-bit delay timer register
+    delay_t: u8,
+    // 8-bit sound timer register
+    sound_t: u8,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// starting address
+const START_ADDR: u16 = 0x200;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Cpu {
+    pub fn new() -> Self {
+        Self {
+            pc: START_ADDR,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_WIDTH * SCREEN_HEIGHT],
+            v_reg: [0; NUM_V_REGS],
+            i_reg: 0,
+            stack: [0; STACK_SIZE],
+            keys: [false; NUM_KEYS],
+            delay_t: 0,
+            sound_t: 0,
+        }
     }
 }
