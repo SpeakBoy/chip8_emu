f0370f69f77c7171e8691a4b9de482a64d331a76
SpeakBoy <nevinspeakman@outlook.com>
2025-05-25 22:00:20 -0400

    Add VX += NN instruction

--- chip8_emu_backend/src/lib.rs
+++ chip8_emu_backend/src/lib.rs
@@ -180,6 +180,12 @@ impl Cpu {
                 let nn = (op & 0xFF) as u8;
                 self.v_reg[x] = nn;
             }
+            // VX += VN
+            (7, _, _, _) => {
+                let x = digit_2 as usize;
+                let nn = (op & 0xFF) as u8;
+                self.v_reg[x] = self.v_reg[x].wrapping_add(nn);
+            }
             (_, _, _, _) => unimplemented!("Unimplemented opcode: {}", op),
         }
     }
