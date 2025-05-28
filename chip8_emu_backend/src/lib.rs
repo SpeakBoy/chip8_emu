06b6380a386913f2bde1aac39f15f485b8b35449
SpeakBoy <nevinspeakman@outlook.com>
2025-05-27 22:31:43 -0400

    Add EX9E instruction

--- chip8_emu_backend/src/lib.rs
+++ chip8_emu_backend/src/lib.rs
@@ -322,6 +322,15 @@ impl Cpu {
                     self.v_reg[0xF] = 0;
                 }
             }
+            // Skip if Key Pressed
+            (0xE, _, 9, 0xE) => {
+                let x = digit_2 as usize;
+                let vx = self.v_reg[x];
+                let key = self.keys[vx as usize];
+                if key {
+                    self.pc += 2;
+                }
+            }
             (_, _, _, _) => unimplemented!("Unimplemented opcode: {}", op),
         }
     }
